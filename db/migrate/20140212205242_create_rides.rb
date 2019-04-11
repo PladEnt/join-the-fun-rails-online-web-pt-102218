c36ae65de0b715828ac988c048755ae37f80ddc6
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-04-11 16:28:54 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      integer :taxi_id
+      integer :passenger_id
       t.timestamps null: false
     end
   end
